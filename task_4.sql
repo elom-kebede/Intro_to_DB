f22b488a15e3c1603bb313a5d11a99184f207cec
elom-kebede <kebedeelias30@gmail.com>
2025-01-31 02:39:31 -0800

    change the keyword to capital

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,5 @@
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, COLUMN_KEY, IS_NULLABLE, COLUMN_DEFAULT
-FROM information_schema.columns
-WHERE table_name = 'books'
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store',TABLE_NAME = 'books'
