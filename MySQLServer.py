c3c3d9ad7f8338495cb161c274ac8ec097a9519e
elom-kebede <kebedeelias30@gmail.com>
2025-01-31 02:03:27 -0800

    change the exeption statment

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,7 +13,7 @@ try:
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
 except Error as e:
-        print(f"Error: {e}")
+        print(f"except mysql.connector.Error")
 cursor.close()
 connection.close()
 
